41cdd4e8ffba4d41b229f1b8daf29a5b04c529c7
Dylan Peralta <dylanperaltasalerno@gmail.com>
2021-11-27 22:27:31 -0300

    primer intento de crear un formulario con php

---
+++ correo.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = 'dylanperaltasalerno@gmail.com';
+    //correo de destino
+    $nombre = $_POST['name'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['message'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde la página de Dylan Peralta";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensaje, $header);
+    echo '<h3>Mensaje enviado exitosamente!</h3>';
+    echo '<script>setTimeout(\'location.href="index.html"\', 1000)</script>';
+
+?>
